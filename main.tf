49cb5ea072e6ed2c2670596a71394e3ed35fa0d3
Swedish Wiking <william@soderberg.tech>
2025-10-20 15:30:31 +0200

    removed second entrypoint

--- main.tf
+++ main.tf
@@ -169,7 +169,6 @@ resource "docker_container" "workspace" {
     "&&", "exec",
     "sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")
   ]
-  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
   env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
   host {
     host = "host.docker.internal"
